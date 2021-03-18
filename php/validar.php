839b915c33c269e81fb7168e3f90eb64e506abd5
stivenshun <73649327+davidstivenmerchan@users.noreply.github.com>
2021-03-17 22:21:29 -0500

    inicio de sesion

---
+++ php/validar.php
@@ -0,0 +1,34 @@
+<?php
+
+$usuario = $_POST['usu'];
+$contraseña = $_POST['pass'];
+
+session_start();
+$_SESSION['usuario'] = $usuario;
+
+include('conexion.php');
+
+$consulta = "SELECT * FROM usuario WHERE usuario = '$usuario' and contrasena = '$contraseña'";
+$resultado = mysqli_query($conexion, $consulta);
+
+$fila=mysqli_num_rows($resultado);
+
+if($fila){
+
+    header("location: ../html/admi.html");
+}else{
+    ?>
+    <?php
+    include('index.html');
+    ?>
+    <h1>error</h1>
+    <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
+
+?>
+
+
+
+
