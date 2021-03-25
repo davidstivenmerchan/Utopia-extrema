ece150baf7daf749ac9cd5542047d2942c837ab0
Daniela8426 <dnieto57@misena.edu.co>
2021-03-24 20:33:46 -0500

    todo se dañoo

--- clientes/clientes.php
+++ clientes/clientes.php
@@ -10,7 +10,7 @@
     include('../php/conexion.php');
 
 
-    $sql ="SELECT codigo.id_compra, codigo.cedula, usuarios.nombre, usuarios.apellido, usuarios.correo, estado.nom_estado, card.name_card, compra.tickest from codigo, usuarios, estado, compra, card WHERE usuarios.cedula=codigo.cedula and estado.id_estados=compra.id_estados and compra.id_compra=codigo.id_compra and card.id_card=compra.id_card and codigo.id_compra='$id_compraa' and codigo.cedula='$c'";
+    $sql ="SELECT codigo.id_compra, codigo.cedula, usuarios.nombre, usuarios.apellido, usuarios.correo, estado.nom_estado, card.name_card, compra.tickest , compra.date_vcto from codigo, usuarios, estado, compra, card WHERE usuarios.cedula=codigo.cedula and estado.id_estados=compra.id_estados and compra.id_compra=codigo.id_compra and card.id_card=compra.id_card and codigo.id_compra='$id_compraa' and codigo.cedula='$c'";
    # $sql ="SELECT usuarios.nombre, usuarios.apellido, usuarios.correo, usuarios.cedula, compra.id_card, compra.tickest, estado.nom_estado FROM usuarios, estado, compra WHERE usuarios.cedula=compra.cedula and estado.id_estados=compra.id_estados and usuarios.cedula=' $cedulaaa'";
     $resultados = mysqli_query($conexion, $sql);
     $fila=mysqli_fetch_array($resultados);
@@ -21,9 +21,8 @@
         
 
        /* $tir = $ti - 1; */
+
     
-
-
     
     ?>
 <?php
@@ -46,11 +45,13 @@
                 echo "<script> alert('Lo sentimos no tiene mas tickest por favor recargue su tarjeta'); </script>";
             }
             
+
             
         }
 
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -72,11 +73,29 @@
     <p>Nombre: <?php echo $fila['nombre'] ?></p>
     <p>Apellido: <?php echo $fila['apellido'] ?></p>
     <p>Correo: <?php echo $fila['correo'] ?></p>
+    <h4>DATOS TARJETA:</h4>
     <p>ID Compra: <?php echo $fila['id_compra'] ?></p>
     <p>Tickest Disponibles:<?php echo $ti ?> </p>
     <p>Tipo de Tajera:<?php echo $fila['name_card'] ?> </p>
     <p>Estado de la Tarjeta: <?php echo $fila['nom_estado'] ?></p>
-  
+     <?php 
+     $hoy = date("Y-m-d");
+     $fechaAc= date_create($hoy);
+     $vencimiento = date_create($fila['date_vcto']);
+     $dias = date_diff($fechaAc,$vencimiento);
+     $dia = $dias->format("%R%a dias");
+     $busca = strpos($dia, "+");
+     
+     if($busca === false){
+         $consultica = "UPDATE compra SET id_estados = 2 WHERE  id_compra = '$id_compraa' ";
+         $ejecutar = mysqli_query($conexion, $consultica);
+     }else{
+        echo "<p>Su tarjeta expira en: $dia </p>";
+     }
+    ?>
+    
+    
+    
 
     <form action="" method="POST">
        <p>maquina 1</p>
