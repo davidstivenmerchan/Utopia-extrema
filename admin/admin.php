87c072c3f11e3b82ec7b18e0599edbe07b440e53
Daniela8426 <dnieto57@misena.edu.co>
2021-03-28 22:20:43 -0500

    creacion de modulo de planeacion de atracciones

--- admin/admin.php
+++ admin/admin.php
@@ -28,6 +28,7 @@ $query3 = mysqli_query($conexion, $sql3);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ADMINISTRADOR</title>
     <link rel="stylesheet" href="estilos.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
@@ -96,10 +97,10 @@ $query3 = mysqli_query($conexion, $sql3);
 
     <div class="vencimiento">
         <?php 
-          if(isset($_POST['buscar'])){
+          if(@$_POST['buscar']){
             $card = $_POST['tipo_card'];
             $estado = $_POST['estado'];
-            $sql2 = "SELECT compra.id_compra, compra.date, card.name_card, usuarios.nombre, usuarios.apellido, usuarios.celular, usuarios.correo, compra.date_vcto FROM compra, card , usuarios WHERE compra.id_card= '$card' and compra.id_estados = '$estado'  and compra.id_card = card.id_card";
+            $sql2 = "SELECT compra.id_compra, compra.date, card.name_card, usuarios.nombre, usuarios.apellido, usuarios.celular, usuarios.correo, compra.date_vcto FROM compra, card , usuarios WHERE compra.id_card= '$card' and compra.id_estados = '$estado'  and compra.id_card = card.id_card and compra.cedula = usuarios.cedula";
             $query2 = mysqli_query($conexion, $sql2);
             
              
@@ -147,9 +148,9 @@ $query3 = mysqli_query($conexion, $sql3);
         ?>
     </div>
 
-    <div class="planificacion">
-        <form action="">
-            <h2>PLANIFICACION</h2>
+    <div id="planeacion">
+        <form action="planeacion.php" method="POST">
+            <h2>PLANEACION</h2>
             <p>Seleccione las maquinas que estaran en servicio el dia de hoy: </p><br>
             <?php 
                 $sql4 = "SELECT atraccion.id_atraccion, tipo_atraccion.nom_tip_atraccion, atraccion.nom_atraccion, ubicacion.nom_ubi FROM atraccion, tipo_atraccion, ubicacion WHERE atraccion.id_tip_atraccion = tipo_atraccion.id_tip_atraccion and atraccion.id_ubi = ubicacion.id_ubi ";
@@ -168,30 +169,108 @@ $query3 = mysqli_query($conexion, $sql3);
             <?php 
                 while($column=mysqli_fetch_array($query4))
                 {
-                    echo'<tbody>';
-                        echo'<tr>';
-                            echo'<td>'.$column[1].'</td>';
-                            echo'<td>'.$column[2].'</td>';
-                            echo'<td>'.$column[3].'</td>';
-                            echo'<td><input type="checkbox" id='.$column[0].'></td>';
-                        echo'</tr>';
-                    echo'</tbody>';
+             ?> 
+                  <tbody>
+                      <tr>
+                          <td><?php echo $column[1]; ?> </td>
+                          <td><?php echo $column[2]; ?> </td>
+                          <td><?php echo $column[3];?> </td>
+                          <td><input type="checkbox" name="atraccion[]" value=<?php echo $column[0]; ?> ></td>
+                      </tr>
+                  </tbody>
 
-                    
+            <?php         
                 }
             ?>  
             </table><br>
             <div class="horas">
                 <label>HORA INICIO:</label>
-                <input type="time"  min="04:00" max="24:00" required>
+                <input type="time" name="h_inicio" min="04:00" max="24:00" required>
                 <label>HORA FIN:</label>
-                <input type="time"  min="04:00" max="24:00" required>
+                <input type="time" name="h_fin" min="04:00" max="24:00" required>
             </div><br>
-            <input type="submit" name="Registrar" value="Registrar"/>
+            <div class="botones">
+              <input type="submit" name="planeacion"  value="Programar" />
+              <input type="hidden" name="cedula" value=<?php echo $fila ['cedula'];?>>
+            </div>
 
         </form>
     </div>   
 
+    <div class="control">
+        <h2>CONTROL DE MAQUINARIA</h2>
+        <form  method="POST">
+            <label for="">Seleccione la fecha a buscar</label>
+            <input type="date" id="fecha" name="fecha"  value="<?php echo date("Y-m-d");?>">
+            <input type="submit" name="Buscar" value= "Buscar">
+        </form>
+      
+        <?php 
+            $buscardor = mysqli_query($conexion,"SELECT atraccion.id_atraccion, atraccion.nom_atraccion, planeacion.fecha_reg, planeacion.h_inicio, planeacion.h_fin FROM atraccion,planeacion WHERE  atraccion.id_atraccion = planeacion.id_atraccion"); 
+            
+        ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Codigo</th>
+                    <th>Ubicación</th>
+                    <th>Hora de inicio</th>
+                    <th>Hora de fin</th>
+                    <th>Hora restante</th>
+                    <th>Hora acumuladas</th>
+                    
+                </tr>
+            </thead>
+        <?php
+
+            while($row=mysqli_fetch_array($buscardor)){
+            date_default_timezone_set('America/Bogota');
+
+            $hora1 = new DateTime(date("H:i:s"));//hora actual
+            $hora2 = new DateTime($row[4]);//hora de cierre
+
+            $intervalo = $hora1->diff($hora2);
+            $tiempoR = $intervalo->format('%R %H horas %i minutos %s segundos');
+            $busca = strpos($tiempoR, "+");
+     
+            if($busca === false){
+
+               /*  $horasT = "SELECT total_h FROM horas_trabajo WHERE id_atraccion = '$row[0]' ";
+                $ejecutar1 = mysqli_query($conexion, $horasT); */
+
+                $hora3 = new DateTime($row[3]);//hora de inicio
+                $intervalo2 = $hora2->diff($hora3);
+                $tiempo = $intervalo2->format('%H:%i:%s');
+
+                /* $total = $hello->date_interval_create_from_date_string($tiempo);
+                $totalH = $total->format("%H:%i:%s"); */
+                
+
+                $consultica = "UPDATE horas_trabajo SET total_h = '$tiempo' WHERE  id_atraccion = '$row[0]' ";
+                $ejecutar2 = mysqli_query($conexion, $consultica);
+                
+        
+
+        ?>
+            <tbody>
+                <tr>
+                    <td><?php echo $row[0]; ?> </td>
+                    <td><?php echo $row[1]; ?> </td>
+                    <td><?php echo $row[3];?> </td>
+                    <td><?php echo $row[4];?> </td>
+                    <td><?php echo $tiempoR;?> </td>
+                    <td><?php echo $tiempo;?> </td>
+                </tr>
+            </tbody>
+
+        <?php         
+          }else{
+        }
+    }
+        ?>  
+      </table>
+
+    </div>
 <?php
 
    $query5=mysqli_query($conexion,"SELECT * FROM tipo_atraccion");
---
+++ admin/planeacion.php
@@ -0,0 +1,35 @@
+<?php 
+require_once("../php/conexion.php");
+
+if (@$_POST['planeacion']){
+   
+    $atraccion = $_POST['atraccion'];
+    $h_inicio = $_POST['h_inicio'];
+    $h_fin = $_POST['h_fin'];
+    $cedula = $_POST['cedula'];
+
+    $Planeacion = "INSERT INTO  planeacion (id_plan, id_atraccion, cedula, fecha_reg, h_inicio, h_fin) VALUES ";
+
+    for ($i=0; $i < count($atraccion); $i++){
+        $Planeacion.= "('','".$atraccion[$i]."', '$cedula', NOW(), '$h_inicio', '$h_fin' ),";
+    }
+
+    $cadena_final = substr($Planeacion, 0, -1);
+    $cadena_final.=";";
+    
+    if($conexion->query($cadena_final)){
+        
+        echo "<script> alert('REGISTRO DE PROGAMACION EXITOSA, QUE TENGA BUEN DIA');
+            window.location= 'admin.php';
+            </script>";
+        exit;
+        
+    }else {
+        echo "<script> alert('ERROR DE INSERCION, PORFAVOR INTENTE NUEVAMENTE');
+            window.location= 'admin.php'</script>";
+        exit;
+    }
+}
+
+
+?>
