653105feecfd9c35048fa76afd7b9f9b3e09c78b
stivenshun <dsmerchan6@misena.edu.co>
2021-03-29 21:09:05 -0500

    contador

--- php/cerrar.php
+++ php/cerrar.php
@@ -1,18 +1,5 @@
 <?php 
-    include('conexion.php');
-
-    if(isset($_POST['salida'])){
-
-        echo "<script> alert('si entra en la condicion de la consulta para ingresar la fecha de salida!'); </script>";
-
-        $fecha_salida = $_POST['fechasalida'];
-
-        $qr = $_POST['qr'];
-
-        $consulta = "UPDATE entry_exit SET fe_ho_exit='$fecha_salida' where id_qr='$qr'";
-        $res = mysqli_query($conexion,$consulta);
-
-    }
+  
 
     session_start();
     if(isset($_COOKIE['session_name()'])){
--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conexion= new mysqli("localhost","root","","comprautopia");
+    $conexion= new mysqli("localhost","root","","comprautopia3");
     if($conexion->connect_error)
     {
         die("fallo la conexion" . mysqli_connect_errno());
